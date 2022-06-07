26756b900ba7ae9a3389e26aa4e9ef34a238d6f7
Alec German <kimarimt@protonmail.com>
2022-06-07 18:38:12 -0400

    finished nato alphabet project

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+import pandas as pd
+
+
+def get_nato():
+    df = pd.read_csv('nato_phonetic_alphabet.csv')
+    return {entry[0]: entry[1] for entry in df.values}
+
+
 def main():
-    print('Hello, World!')
+    word = input('Enter a word: ').upper()
+    nato_scheme = get_nato()
+    conversion = [nato_scheme[letter] for letter in word]
+    print(conversion)
 
 
 if __name__ == '__main__':
